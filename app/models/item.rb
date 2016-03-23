64d02188dedea66edae895440f38b9262893fa5b
tvlad <tvl71@mail.ru>
2016-03-23 16:38:06 +0200

    eps. 6

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveRecord::Base
   
   validates :price, numericality: {greater_than: 0, allow_nil: true}
   validates :name, :description, presence: true
+  
+  belongs_to :category
+  
+  after_initialize {puts "initialized"} # Item.new; Item.first
+  after_save       {puts "saved"}  # item.save; item.create; item.update
+  after_create {category.inc(:items_count, 1)}
+  after_update {puts "updated"}
+  after_destroy {category.inc(:items_count, -1)}
 end
