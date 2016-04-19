b4ad5d1f6db100b1825c4d7fe46dbd33c8108135
tvlad <tvl71@mail.ru>
2016-04-19 16:01:22 +0300

    episode 23 (add comments) polymorphic relations

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ActiveRecord::Base
   
   has_many :positions
   has_many :carts, through: :positions
+  has_many :comments, as: :commentable
   
   after_initialize {puts "initialized"} # Item.new; Item.first
   after_save       {puts "saved"}  # item.save; item.create; item.update
