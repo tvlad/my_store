b663df07735fde45d4665bde2fb60ecb1348ea04
tvlad <tvl71@mail.ru>
2016-05-12 17:02:06 +0300

    ep. 29

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,11 @@ class ItemsController < ApplicationController
   def index
     #    @items =  Item.all
     #    render text: @items.map{|i| "#{i.id}. Name - #{i.name}, Price - #{i.price}"}.join("<br/>")
-    @items =  Item.all.order('votes_count desc', 'price desc').limit(3)
+    @items =  Item
+    @items = @items.where("price >= ?", params[:price_from]) if params[:price_from]
+    @items = @items.where("created_at >= ?", 1.day.ago) if params[:today]
+    @items = @items.where("votes_count >= ?", params[:votes_from]) if params[:votes_from]
+    @items = @items.order('votes_count desc', 'price desc').limit(50)
   end
   
   # /items/1 GET
