302e245cb91a3265fbd3d61576e57d36d4f6ab0e
tvlad <tvl71@mail.ru>
2016-06-01 15:23:48 +0300

    ep. 42

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 describe Order do
   
-  it "calculates the total price of the order" do
+  it 'calculates the total price of the order' do
     item1 = Item.new(price: 100)
     item2 = Item.new(price: 200)
     
@@ -15,16 +15,23 @@ describe Order do
     expect(order.total).to  eq(300)
   end
 
-  it "calculates the total price of the order using factory" do
+  it 'calculates the total price of the order using factory' do
+    # i removed FactoryGirl and had include this method to <rails_helper> file
     item1 = create(:item)
     item2 = create(:item, price: 20)
 
-    order = create(:order)
+    # it doesn't work after adding <before_save> to the <order> model
+    # order = create(:order)
+    order = Order.new()
     order.items << item1
     order.items << item2
 
     order.calculate_total
     expect(order.total).to  eq(30)
+  end
+
+  it 'raises exception if an order has no items in it' do
+    expect(-> {create(:order)}).to raise_exception
   end
 
 end
