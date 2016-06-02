d5a21b1ac3cdc08e626c7b83c37832b8b148933d
tvlad <tvl71@mail.ru>
2016-06-02 13:18:24 +0300

    ep. 44 test for controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -19,4 +19,25 @@ describe ItemsController do
 
   end
 
+  describe 'create action' do
+
+    it 'redirect to the item path if validation passed' do
+      post :create, item: {name: 'Item1', description: 'description', price: '10'}
+
+      expect(response).to redirect_to item_path(assigns(:item))
+
+    end
+
+    it 'render the new page again if price is eq. nill' do
+      post :create, item: { name: 'Item1', description: 'description', price: nil}
+      expect(response).to render_template('new')
+    end
+
+    it 'render the new page again if price is eq. 0' do
+      post :create, item: { name: 'Item1', description: 'description', price: 0}
+      expect(response).to render_template('new')
+    end
+
+  end
+
 end
