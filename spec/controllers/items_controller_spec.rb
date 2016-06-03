6d9c3bbaf76811eeeb884f5ccfe91be5f9afbbc7
tvlad <tvl71@mail.ru>
2016-06-03 14:28:24 +0300

    ep. 45

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 require 'rails_helper'
 
-
 describe ItemsController do
+
+  def self.it_renders_404_if_page_not_found(*actions)
+    actions.each do |a|
+      it "#{a} render 404 page" do
+        verb = if a == :update
+                 "PATCH"
+               elsif a == :destroy
+                 "DELETE"
+               else
+                 "GET"
+               end
+        process a, verb, {id: 0}
+        expect(response.status).to eq 404
+      end
+    end
+  end
+
+  it_renders_404_if_page_not_found :show, :edit, :update, :destroy
 
   describe 'show action' do
 
@@ -12,10 +29,10 @@ describe ItemsController do
       expect(response).to render_template(:show)
     end
 
-    it 'renders 404 page if an item is not found' do
-      get :show, id: 0
-      expect(response.status).to eq 404
-    end
+    # it 'renders 404 page if an item is not found' do
+    #   get :show, id: 0
+    #   expect(response.status).to eq 404
+    # end
 
   end
 
@@ -37,6 +54,21 @@ describe ItemsController do
       post :create, item: { name: 'Item1', description: 'description', price: 0}
       expect(response).to render_template('new')
     end
+
+  end
+
+  describe 'destroy action' do
+
+    it 'redirect to index after item has been destroyed' do
+      item = create(:item)
+      delete :destroy, id: item.id
+      expect(response).to redirect_to items_path
+    end
+
+    # it 'action destroy render 404-page if the item was not found' do
+    #   delete :destroy, id: 0
+    #   expect(response.status).to eq 404
+    # end
 
   end
 
